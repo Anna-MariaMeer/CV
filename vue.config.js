36210f22c1365f2b2c2f70918a61c99fc7efbed1
AnnaMariaMeer <anna.meer@rwth-aachen.de>
2022-09-22 17:33:08 +0200

    correct vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath      : process.env.NODE_ENV === 'production' ? '/Resume/' : '/',
+  publicPath      : process.env.NODE_ENV === 'production' ? '/CV/' : '/',
   css             : { loaderOptions: { css: { url: false } } },
   configureWebpack: (config) => {
     config.module.rules = [
